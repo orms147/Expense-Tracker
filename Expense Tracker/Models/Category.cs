f8e4837a8909072d7c2b4e35d1f6f655859c6936
Nguyen Ha Bach <orms147@gmail.com>
2024-08-29 15:29:16 +0700

    MVC update, link to DB

---
+++ Expense Tracker/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key] public int CategoryId { get; set; }
+
+
+        [Column(TypeName = "nvarchar(50)")]
+        [Required] public string Title { get; set; }
+
+
+        [Column(TypeName = "nvarchar(10)")]
+        [Required] public string Type { get; set; } = "Expense";
+
+
+        [Column(TypeName = "nvarchar(10)")]
+        [Required] public string Icon { get; } = "";
+    }
+}
--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -1,7 +1,15 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DI
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"))
+    );
 
 var app = builder.Build();
 
@@ -18,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Category}/{action=Index}/{id?}");
 
 app.Run();
